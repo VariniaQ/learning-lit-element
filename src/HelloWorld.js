8b9d2ba26a31abc7f6d1ddb081ae566f70784efa
VariniaQ <quisbertvarinia@gmail.com>
2022-06-10 22:32:04 -0300

    add first file to create a component

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+import { LitElement, html } from 'lit-element';
+
+class HelloWorld extends LitElement {
+    render() {
+        return html`
+            
+        `;
+    }
+}
+customElements.define('hello-world', HelloWorld);
